218f971279a21b6cf211e959cd7c6c2787451561
John <jastadj@gmail.com>
2019-02-14 15:39:56 -0800

    version 0.1

--- Levels/level01.gd
+++ Levels/level01.gd
@@ -2,4 +2,4 @@ extends "res://Levels/level_base.gd"
 
 func _init():
 	# set the next level
-	next_level = null
+	next_level = "res://Levels/level02.tscn"
---
+++ Levels/level02.gd
@@ -0,0 +1,6 @@
+extends "res://Levels/level_base.gd"
+
+func _init():
+	# set the next level
+	level_time = 60*3
+	next_level = "res://Levels/level03.tscn"
