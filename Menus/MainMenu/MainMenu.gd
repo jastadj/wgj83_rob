7526adf5c0701c954936832fdbb29b722936172f
John <jastadj@gmail.com>
2019-02-13 23:17:50 -0800

    main menu update

--- Menus/MainMenu/MainMenu.gd
+++ Menus/MainMenu/MainMenu.gd
@@ -1,13 +1,24 @@
 extends CanvasLayer
 
+var image_fade_speed = 0.3
+
 func _ready():
 	$NewGameButton.connect("pressed", self, "new_game")
 	$QuitButton.connect("pressed", self, "quit_game")
+	fade_title_image()
+
+
+func fade_title_image():
+	
+	for step in range(0,5):
+		var timer = get_tree().create_timer(image_fade_speed)
+		yield(timer, "timeout")
+		$TitleImageCover.modulate.a = $TitleImageCover.modulate.a - 0.25
 
 func new_game():
 	$ClickSound.play()
 	randomize()
-	get_tree().change_scene("res://Levels/level_test.tscn")
+	get_tree().change_scene("res://Levels/level01.tscn")
 	
 func quit_game():
 	$ClickSound.play()
